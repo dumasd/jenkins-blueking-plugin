3d379a6cc5ad5d1a2150bd14c00825f7058e59ca
Bruce.wu <bruce.wu@optnet.site>
2024-07-12 09:51:58 +0800

    修改插件配置

--- src/main/java/io/jenkins/plugins/blueking/config/PropertiesEnvConfig.java
+++ src/main/java/io/jenkins/plugins/blueking/config/PropertiesEnvConfig.java
@@ -4,27 +4,47 @@ import hudson.Extension;
 import hudson.model.Describable;
 import hudson.model.Descriptor;
 import hudson.util.FormValidation;
+
 import java.io.Serializable;
+
 import jenkins.model.Jenkins;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 import org.apache.commons.lang.StringUtils;
 import org.kohsuke.stapler.DataBoundConstructor;
+import org.kohsuke.stapler.DataBoundSetter;
 import org.kohsuke.stapler.QueryParameter;
 
 @Setter
 @Getter
 @NoArgsConstructor
 public class PropertiesEnvConfig implements Serializable, Describable<PropertiesEnvConfig> {
-
+    /**
+     * properties 文件中的key
+     */
     private String key;
+
+    /**
+     * properties value写入的环境变量名称
+     */
     private String env;
+
+    /**
+     * properties value
+     */
+    private String defaultValue;
+
 
     @DataBoundConstructor
     public PropertiesEnvConfig(String key, String env) {
         this.key = key;
         this.env = env;
+    }
+
+    @DataBoundSetter
+    public void setDefaultValue(String defaultValue) {
+        this.defaultValue = defaultValue;
     }
 
     @Override
